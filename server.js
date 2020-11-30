42c3c41ae9087f87edcc7d8db8f2596cd2213077
snh3003 <shahsama542@gmail.com>
2020-12-01 00:10:51 +0530

    postgresql connection added

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ app.use(cors());
 const postgres = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'snh3003',
-    database : 'smart'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
